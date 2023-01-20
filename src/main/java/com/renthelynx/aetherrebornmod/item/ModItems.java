02fd841d2cb8fd5e859aa3d848af14db13f43bf2
DancerVlt69 <dancervlt69@googlemail.com>
2023-01-21 00:29:11 +0100

    changed/cleanup: simplify using MOD_ID | a bit code cleanup (only cosmetics)

--- src/main/java/com/renthelynx/aetherrebornmod/item/ModItems.java
+++ src/main/java/com/renthelynx/aetherrebornmod/item/ModItems.java
@@ -1,27 +1,26 @@
 package com.renthelynx.aetherrebornmod.item;
 
-import com.renthelynx.aetherrebornmod.AetherRebornMod;
-
 import net.minecraft.world.item.Item;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.RegistryObject;
 
+import static com.renthelynx.aetherrebornmod.AetherRebornMod.MOD_ID;
+
 public class ModItems {
-    public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, AetherRebornMod.MOD_ID);
-    
+    public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, MOD_ID);
+
     // ITEMS DEFINITION
-        public static final RegistryObject<Item> SKYROOTSTICK = ITEMS.register("skyrootstick", () -> new Item(new Item.Properties().tab(ModCreativeModeTab.AETHREB_TAB)));
+        public static final RegistryObject<Item> SKYROOTSTICK = ITEMS.register("skyrootstick",
+            () -> new Item(new Item.Properties().tab(ModCreativeModeTab.AETHREB_TAB)));
         //public static final RegistryObject<Item> SKYROOTSTICK = ITEMS.register("skyrootstick", () -> new Item(new Item.Properties().tab(ModCreativeModeTab.AETHREB_TAB)));
 
     // END DEFINITION
-    
-    public static void register(IEventBus eventBus) {
-        ITEMS.register(eventBus);
-    }
-    
-    
+
+    public static void register(IEventBus eventBus) { ITEMS.register(eventBus); }
+
+
 
 
 
