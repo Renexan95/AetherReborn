56b8d9dcee8437fc145eca1404d1b79331b1fbc9
DancerVlt69 <dancervlt69@googlemail.com>
2023-01-21 00:17:06 +0100

    changed/cleanup: simplified using MOD_ID | call ModFlammableBlock instead Block, added the now needed Parameter

--- src/main/java/com/renthelynx/aetherrebornmod/block/ModBlocks.java
+++ src/main/java/com/renthelynx/aetherrebornmod/block/ModBlocks.java
@@ -1,22 +1,18 @@
 package com.renthelynx.aetherrebornmod.block;
 
-import com.renthelynx.aetherrebornmod.AetherRebornMod;
+import com.renthelynx.aetherrebornmod.block.custom.ModFlammableBlock;
 import com.renthelynx.aetherrebornmod.block.custom.ModFlammableRotatedPillarBlock;
 import com.renthelynx.aetherrebornmod.item.ModCreativeModeTab;
 import com.renthelynx.aetherrebornmod.item.ModItems;
 
-import net.minecraft.core.BlockPos;
-import net.minecraft.core.Direction;
 import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.Item;
-import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.FenceBlock;
 import net.minecraft.world.level.block.StairBlock;
 import net.minecraft.world.level.block.state.BlockBehaviour;
-import net.minecraft.world.level.block.state.BlockState;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
@@ -24,47 +20,61 @@ import net.minecraftforge.registries.RegistryObject;
 
 import java.util.function.Supplier;
 
+import static com.renthelynx.aetherrebornmod.AetherRebornMod.MOD_ID;
+
 public class ModBlocks {
-    public static final DeferredRegister<Block> BLOCKS = DeferredRegister.create(ForgeRegistries.BLOCKS, AetherRebornMod.MOD_ID);
-    
+    public static final DeferredRegister<Block> BLOCKS = DeferredRegister.create(ForgeRegistries.BLOCKS, MOD_ID);
+
     // BLOCK DEFINITION
 
+        /** If you add a constructor into your ModFlammableRotatedPillarBlock <p>
+         * you can give all registered Block their own flammable-Attributes</p>*/
         // Skyroot Blocks
         public static final RegistryObject<Block> SKYROOT_LOG = registerBlock("skyroot_log",
-            () -> new ModFlammableRotatedPillarBlock(BlockBehaviour.Properties.copy(Blocks.OAK_LOG)),
-            ModCreativeModeTab.AETHREB_TAB);
+            () -> new ModFlammableRotatedPillarBlock(BlockBehaviour.Properties.copy(Blocks.OAK_LOG),
+                    true, 5,5), ModCreativeModeTab.AETHREB_TAB);
+
         public static final RegistryObject<Block> SKYROOT_WOOD = registerBlock("skyroot_wood",
-            () -> new ModFlammableRotatedPillarBlock(BlockBehaviour.Properties.copy(Blocks.OAK_WOOD)),
-            ModCreativeModeTab.AETHREB_TAB);
+            () -> new ModFlammableRotatedPillarBlock(BlockBehaviour.Properties.copy(Blocks.OAK_WOOD),
+                    true, 5, 5), ModCreativeModeTab.AETHREB_TAB);
+
         public static final RegistryObject<Block> STRIPPED_SKYROOT_LOG = registerBlock("stripped_skyroot_log",
-            () -> new ModFlammableRotatedPillarBlock(BlockBehaviour.Properties.copy(Blocks.STRIPPED_OAK_LOG)),
+            () -> new ModFlammableRotatedPillarBlock(BlockBehaviour.Properties.copy(Blocks.STRIPPED_OAK_LOG),
+                    true, 5, 5),
             ModCreativeModeTab.AETHREB_TAB);
+
         public static final RegistryObject<Block> STRIPPED_SKYROOT_WOOD = registerBlock("stripped_skyroot_wood",
-            () -> new ModFlammableRotatedPillarBlock(BlockBehaviour.Properties.copy(Blocks.STRIPPED_OAK_WOOD)),
-            ModCreativeModeTab.AETHREB_TAB);
+            () -> new ModFlammableRotatedPillarBlock(BlockBehaviour.Properties.copy(Blocks.STRIPPED_OAK_WOOD),
+                    true, 5, 5), ModCreativeModeTab.AETHREB_TAB);
+
+        /** If you create your own FlammableBlock, you can give all the (Wooden) Blocks their own<p>
+         * flammable-Attributes, your code looks much cleaner, and you can save a lot of space</p>*/
         public static final RegistryObject<Block> SKYROOT_PLANKS = registerBlock("skyroot_plank",
-            () -> new Block(BlockBehaviour.Properties.copy(Blocks.OAK_PLANKS)) {
-                @Override
-                public boolean isFlammable(BlockState state, BlockGetter world, BlockPos pos, Direction face) {
-                    return true;
-                }
+            () -> new ModFlammableBlock(BlockBehaviour.Properties.copy(Blocks.OAK_PLANKS),
+                    true, 20, 5), ModCreativeModeTab.AETHREB_TAB);
 
-                @Override
-                public int getFlammability(BlockState state, BlockGetter world, BlockPos pos, Direction face) {
-                    return 20;
-                }
+//            {
+//                @Override
+//                public boolean isFlammable(BlockState state, BlockGetter world, BlockPos pos, Direction face) {
+//                    return true;
+//                }
+//
+//                @Override
+//                public int getFlammability(BlockState state, BlockGetter world, BlockPos pos, Direction face) {
+//                    return 20;
+//                }
+//
+//                @Override
+//                public int getFireSpreadSpeed(BlockState state, BlockGetter world, BlockPos pos, Direction face) {
+//                    return 5;
+//                }
+//            }
 
-                @Override
-                public int getFireSpreadSpeed(BlockState state, BlockGetter world, BlockPos pos, Direction face) {
-                    return 5;
-                }
-            }, ModCreativeModeTab.AETHREB_TAB);
         public static final RegistryObject<Block> SKYROOT_STAIRS = registerBlock("skyroot_stairs",
-            () -> new StairBlock(() -> ModBlocks.SKYROOT_PLANKS.get().defaultBlockState(), BlockBehaviour.Properties.copy(Blocks.OAK_STAIRS)),
-            ModCreativeModeTab.AETHREB_TAB);
+            () -> new StairBlock(() -> ModBlocks.SKYROOT_PLANKS.get().defaultBlockState(),
+                    BlockBehaviour.Properties.copy(Blocks.OAK_STAIRS)), ModCreativeModeTab.AETHREB_TAB);
         public static final RegistryObject<Block> SKYROOT_FENCE = registerBlock("skyroot_fence",
-            () -> new FenceBlock(BlockBehaviour.Properties.copy(Blocks.OAK_FENCE)),
-            ModCreativeModeTab.AETHREB_TAB);
+            () -> new FenceBlock(BlockBehaviour.Properties.copy(Blocks.OAK_FENCE)), ModCreativeModeTab.AETHREB_TAB);
         //Whisproot Blocks
         //Goldentree Blocks
 
@@ -78,8 +88,8 @@ public class ModBlocks {
         return toReturn;
     }
 
-    private static <T extends Block> RegistryObject<Item> registerBlockItem(String name, RegistryObject<T> block, CreativeModeTab tab) {
-        return ModItems.ITEMS.register(name, () -> new BlockItem(block.get(),
+    private static <T extends Block> void registerBlockItem(String name, RegistryObject<T> block, CreativeModeTab tab) {
+        ModItems.ITEMS.register(name, () -> new BlockItem(block.get(),
                 new Item.Properties().tab(tab)));
     }
 
